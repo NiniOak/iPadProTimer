a05660d3799b71a16a2327ba379d6192fef60b89
Anita <anita@Students-MacBook-Pro.local>
2017-02-09 10:17:20 -0500

    Created a timer to be displayed on iPad and iPhone

--- secondlab/ViewController.m
+++ secondlab/ViewController.m
@@ -12,11 +12,33 @@
 
 @end
 
+NSTimer *t;
+
 @implementation ViewController
+- (IBAction)startTimer:(id)sender {
+    t = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(onTick:) userInfo:nil repeats:YES];
+}
+
+//Stop Button
+- (IBAction)stopTimer:(id)sender {
+    if (t) {
+        [t invalidate];
+        t = nil;
+    }
+}
+
+
+-(void)onTick:(NSTimer *)timer {
+    _m_cost += 0.01;
+    _m_costLabel.text = [NSString stringWithFormat:@"$%.02f", _m_cost];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor blackColor];
+    
+    _m_cost = 0.00;
 }
 
 
